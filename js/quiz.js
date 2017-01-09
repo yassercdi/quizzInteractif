013ed25159576c939abec14d3bded453055460ca
yasser <yasser.azaiez@nordnet.fr>
2017-01-09 23:17:30 +0100

    creations fichiers de bases

---
+++ js/quiz.js
@@ -0,0 +1,15 @@
+// Liste des questions à afficher. Une question est définie par son énoncé et sa réponse
+var questions = [
+    {
+        enonce: "Combien font 2+2 ?",
+        reponse: "2+2 = 4"
+    },
+    {
+        enonce: "En quelle année Christophe Colomb a-t-il découvert l'Amérique ?",
+        reponse: "1492"
+    },
+    {
+        enonce: "On me trouve 2 fois dans l'année, 1 fois dans la semaine, mais pas du tout dans le jour... Qui suis-je ?",
+        reponse: "La lettre N"
+    }
+];
